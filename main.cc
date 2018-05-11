6459b1ccdf71967ad0a488523d25df6ff50eb03f
hilorrk <hilorrk@gmail.com>
2018-05-12 00:43:02 +0800

    pass bytesize and cnt as program arguments

--- main.cc
+++ main.cc
@@ -7,30 +7,29 @@
 #include "socket/comm.h"
 #endif
 
-#define BYTESIZE 1000
-#define CNT 100000
-
 using namespace std::chrono_literals; 
 int main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        std::cout << "usage: " << argv[0] << " this_host_id hosts..." << std::endl;
+    if (argc < 5) {
+        std::cout << "usage: " << argv[0] << " bytesize cnt this_host_id hosts..." << std::endl;
         exit(1);
     }
-    std::cout << "Byte Size: " << BYTESIZE << " CNT: " << CNT << std::endl;
-    int this_host = atoi(argv[1]);
-    char **hosts = &argv[2];
-    int numhosts = argc - 2;
+    int bytesize = atoi(argv[1]);
+    int cnt = atoi(argv[2]);
+    int this_host = atoi(argv[3]);
+    char **hosts = &argv[4];
+    int numhosts = argc - 4;
+    std::cout << "bytesize: " << bytesize << " cnt: " << cnt << std::endl;
     Comm comm(this_host, hosts, numhosts);
-    Bytes bytes(BYTESIZE, 0);
+    Bytes bytes(bytesize, 0);
     for (int i = 0; i < 10; ++i) {
         std::this_thread::sleep_for(600ms);
-        for (int c = 0; c < CNT; ++c) {
+        for (int c = 0; c < cnt; ++c) {
             for(int h = 0; h < numhosts; ++h) {
                 comm.Cmd(h, bytes);
             }
         }
-        comm.Sync(CNT);
+        comm.Sync(cnt);
         std::cout << "end of iteration " << i << std::endl;
     }
     comm.Finish();
